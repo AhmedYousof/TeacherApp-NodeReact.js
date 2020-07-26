3323d4d326bbc99541c11f61c6674fa690bf8dd9
AhmedYousof <31749142+AhmedYousof@users.noreply.github.com>
2020-07-26 16:18:53 +0200

    Create types.js

---
+++ client/src/actions/types.js
@@ -0,0 +1,10 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const GET_COURSES = "GET_COURSES";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_PROFILES = "GET_PROFILES";
+export const GET_STUDENTS = "GET_STUDENTS";
+export const GET_TEACHERS = "GET_TEACHERS";
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_LOADING = "PROFILE_LOADING";
+export const JOIN_COURSE = "JOIN_COURSE";
+export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
